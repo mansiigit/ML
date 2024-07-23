ab572befacb2e61979b08a224b76ceecbd9a7337
helexrox <ankithtech2003@gmail.com>
2024-07-23 20:10:50 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -4,12 +4,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from numpy.linalg import inv
 
-# Kernel function
 def kernel(point, xmat, k):
     weights = np.exp(np.sum((point - xmat)**2, axis=1) / (-2.0 * k**2))
     return np.diag(weights)
 
-# Local weighted regression
 def local_weight_regression(xmat, ymat, k):
     m = xmat.shape[0]
     ypred = np.zeros(m)
@@ -19,16 +17,13 @@ def local_weight_regression(xmat, ymat, k):
         ypred[i] = xmat[i] @ W
     return ypred
 
-# Load and prepare data
 data = sns.load_dataset("tips")
 X = np.hstack((np.ones((len(data), 1)), np.array(data.total_bill).reshape(-1, 1)))
 ytip = np.array(data.tip).reshape(-1, 1)
 
-# Perform local weighted regression
 k = 0.5
 ypred = local_weight_regression(X, ytip, k)
 
-# Plot
 sorted_indices = X[:, 1].argsort()
 plt.scatter(data.total_bill, data.tip, color='green')
 plt.plot(X[sorted_indices, 1], ypred[sorted_indices], color='red', linewidth=2)

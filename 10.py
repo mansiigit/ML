9c7193d647ac78f74322ee8fa5faaac71379e5b1
helexrox <ankithtech2003@gmail.com>
2024-07-23 20:11:07 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -4,11 +4,9 @@ from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
 
-
 iris = datasets.load_iris()
 X = iris.data[:, :2]  
 y = iris.target
-
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
@@ -16,7 +14,6 @@ svm = SVC(kernel='linear', C=1.0, random_state=42)
 svm.fit(X_train, y_train)
 
 y_pred = svm.predict(X_test)
-
 
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)

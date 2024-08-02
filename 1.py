6f36ef3ab213cc3288064b8ab5f98461c2e8e3c6
helexrox <ankithtech2003@gmail.com>
2024-08-02 19:17:27 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,5 +1,5 @@
-//Program: For a given set of training data examples stored in a .CSV file, implement and demonstrate the Find-S algorithm to output a description of the set of all hypotheses
-//consistent with the training examples.
+#Program: For a given set of training data examples stored in a .CSV file, implement and demonstrate the Find-S algorithm to output a description of the set of all hypotheses 
+#consistent with the training examples.
     
 import csv
 

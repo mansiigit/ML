b3039d0f0cf3c9ba0d4d0c1d8cfb1b46fc5d980a
helexrox <ankithtech2003@gmail.com>
2024-08-02 19:22:05 +0530

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,3 +1,5 @@
+#Program: Implement the non-parametric Locally Weighted Regression algorithm in order to fit data points. Select appropriate data set for your experiment and draw graphs.
+
 import numpy as np
 from ipywidgets import interact
 from bokeh.plotting import figure, show, output_notebook

9e83c34ca55fce5a79f4d1ac06d7523ae8615af3
helexrox <ankithtech2003@gmail.com>
2024-08-02 19:20:45 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,3 +1,5 @@
+#Program:- Write a program to construct a Bayesian network considering medical data. Use this model to demonstrate the diagnosis of heart patients using standard Heart Disease Data Set. You can use Python ML library classes/API.
+
 import numpy as np
 import pandas as pd
 from pgmpy.estimators import MaximumLikelihoodEstimator
